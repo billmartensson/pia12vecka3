4c3be9b902da27a068468c3674f5fb7f9015b410
Bill Mårtensson <bill@magictechnology.se>
2023-09-21 09:51:43 +0200

    Mer bra text

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app titta det funkar!</Text>
+      <Text>Mera text</Text>
       <StatusBar style="auto" />
     </View>
   );
