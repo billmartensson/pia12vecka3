e1b3772e630407be56948d661e6c83e20202a3bb
billmartensson <bill@magictechnology.se>
2023-09-21 10:22:30 +0200

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app titta det funkar!</Text>
-      <Text>Mera text nu med en förändring ytterligare text</Text>
+      <Text>Mera text nu med en förändring ytterligare text... oj vad mycket text</Text>
       <StatusBar style="auto" />
     </View>
   );
